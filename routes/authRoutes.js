6b358b3d461fa707511ac040becf8a573f377107
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-05-27 14:07:05 -0400

    logout a user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,8 +10,17 @@ module.exports = app => {
 
   app.get("/auth/google/callback",
     passport.authenticate("google"))
+//kill cookies
+  app.get("/api/logout", (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
 
   app.get("/api/current_user", (req, res) => {
     res.send(req.user)
   })
 }
+//Test steps:
+//1 go to - http://localhost:5000/auth/google - you should see "Cannot GET /auth/google/callback", fix it later
+//2 see user ID - http://localhost:5000/api/current_user
+//3 logout - http://localhost:5000/api/logout - clean window
