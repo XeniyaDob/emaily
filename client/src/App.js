3f2ee56241a0e20313c72b66440c96c338cd4778
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-05-30 16:01:23 -0400

    Setup proxy

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>EMAILY</h1>
+        <a href="/auth/google">Sign In With Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
