4fe4fcfb0480369cd37c1ed5f8e7179e1e77462a
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-06-03 16:30:15 -0400

    Redux Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import App from "./components/App";
+import reducers from "./reducers";
+
+//createStore() isn't a real deprecation as its only purpose is
+//to encourage the use of Redux Toolkit (which introduces configureStore)
+//instead of using createStore directly. This is purposeful decision to use
+// plain React Redux without any abstractions like Redux Toolkit.
+const store = createStore(reducers, {}, applyMiddleware());
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
