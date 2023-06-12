10b425830cfee1427424123685586f088fa55eeb
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-06-12 16:34:01 -0400

    Setup the Header content

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,14 @@
-export default function (state = {}, action) {
-  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!", action);
+import { FETCH_USER } from "../actions/types";
+//Ensure that we have one of 3 values:
+//Null - indicates we really don’t know what’s up right now
+
+//Payload Obj - containing the user ID
+
+//False means - yep, we are sure the user isn’t logged in
+export default function (state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
