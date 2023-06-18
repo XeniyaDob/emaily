78bee00b6d5aadb38ad142c659e5d7602cf43c6c
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-06-18 14:31:28 -0400

    Payments component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily" // the pop-in header title
+        description="$5 for 5 email credits" // the pop-in header subtitle
+        amount={500} //default dollars, 500 cents
+        token={(token) => console.log("Stripe Token", token)} //expecting to received callback func, token from stripe
+        stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}>
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+export default Payments;
