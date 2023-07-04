37f92f5db0fd80ba057a22472410264b04b29261
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-07-04 10:46:32 -0400

    Routing in production

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ app.use(passport.session())
 require("./routes/authRoutes")(app)
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets
+  //like our main.js, or main.css file!!!
+  app.use(express.static("client/build"));
+  //Express will serve up the index.html file
+  //if it doesn't recognize a route
+  const path = require("path");
+  app.get(
+    "*",
+    (req,
+    (res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+  );
+}
+
 const port = process.env.PORT || 5000;
 // Listen on `port` and 0.0.0.0
 app.listen(port, "0.0.0.0", function() {
