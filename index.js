0641b1ff5dfe33e6bce948917e792f003a5d3ecb
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-07-04 11:51:35 -0400

    Routing in Production error fix

--- index.js
+++ index.js
@@ -35,13 +35,10 @@ if (process.env.NODE_ENV === "production") {
   //Express will serve up the index.html file
   //if it doesn't recognize a route
   const path = require("path");
-  app.get(
-    "*",
-    (req,
-    (res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+ 
 }
 
 const port = process.env.PORT || 5000;
