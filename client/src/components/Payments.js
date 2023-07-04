cd14ebaf1761ccb9992fb3db43e7fdb912cfe310
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-07-04 13:09:30 -0400

    Fix STRIPE_PUBLISHABLE_KEY

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,14 +6,16 @@ import * as action from "../actions";
 class Payments extends Component {
   render() {
     return (
-      <StripeCheckout
-        name="Emaily" // the pop-in header title
-        description="$5 for 5 email credits" // the pop-in header subtitle
-        amount={500} //default dollars, 500 cents
-        token={(token) => this.props.handleToken(token)} //expecting to received callback func, token from stripe
-        stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}>
-        <button className="btn">Add Credits</button>
-      </StripeCheckout>
+      <>
+        <StripeCheckout
+          name="Emaily" // the pop-in header title
+          description="$5 for 5 email credits" // the pop-in header subtitle
+          amount={500} //default dollars, 500 cents
+          token={(token) => this.props.handleToken(token)} //expecting to received callback func, token from stripe
+          stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}>
+          <button className="btn">Add Credits</button>
+        </StripeCheckout>
+      </>
     );
   }
 }
